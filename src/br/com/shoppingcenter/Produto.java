ab94df39751e2abd6cc131bd09bf3d2e418e9200
Lucas Souza <www.luucas@gmail.com>
2025-10-18 19:27:24 -0300

    Atualização da classe Produto

--- src/br/com/shoppingcenter/Produto.java
+++ src/br/com/shoppingcenter/Produto.java
@@ -3,10 +3,13 @@ package br.com.shoppingcenter;
 public class Produto {
     private String nome;
     private double preco;
+    private Data dataValidade;
 
-    public Produto(String nome, double preco) {
+
+    public Produto(String nome, double preco, Data dataValidade) {
         this.nome = nome;
         this.preco = preco;
+        this.dataValidade = dataValidade;
     }
 
     public String getNome() { return nome; }
@@ -15,8 +18,28 @@ public class Produto {
     public double getPreco() { return preco; }
     public void setPreco(double preco) { this.preco = preco; }
 
+    public Data getDataValidade() { return dataValidade; }
+    public void setDataValidade(Data dataValidade) { this.dataValidade = dataValidade; }
+
+
     @Override
     public String toString() {
-        return "Produto: " + nome + " | Preço: R$" + preco;
+        return "Produto: " + nome +
+                " | Preço: R$" + preco +
+                " | Validade: " + dataValidade;
     }
+
+    public boolean estaVencido(Data dataAtual) {
+        if (this.dataValidade.getAno() < dataAtual.getAno()) {
+            return true;
+        } else if (this.dataValidade.getAno() == dataAtual.getAno()) {
+            if (this.dataValidade.getMes() < dataAtual.getMes()) {
+                return true;
+            } else if (this.dataValidade.getMes() == dataAtual.getMes()) {
+                return this.dataValidade.getDia() < dataAtual.getDia();
+            }
+        }
+        return false;
+    }
+
 }
