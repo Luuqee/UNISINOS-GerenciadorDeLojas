a3d907451d3e113b221e86322087780e6c2de353
Lucas Souza <www.luucas@gmail.com>
2025-10-18 19:23:10 -0300

    Atualização da classe Loja e adição do Validador Etapa 2

--- src/br/com/shoppingcenter/Loja.java
+++ src/br/com/shoppingcenter/Loja.java
@@ -4,17 +4,23 @@ public class Loja {
     private String nome;
     private int quantidadeFuncionarios;
     private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataFundacao;
 
-    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao) {
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
     }
 
-    public Loja(String nome, int quantidadeFuncionarios) {
+    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao) {
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = -1;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
     }
 
     public String getNome() { return nome; }
@@ -26,11 +32,20 @@ public class Loja {
     public double getSalarioBaseFuncionario() { return salarioBaseFuncionario; }
     public void setSalarioBaseFuncionario(double salarioBaseFuncionario) { this.salarioBaseFuncionario = salarioBaseFuncionario; }
 
+    public Endereco getEndereco() { return endereco; }
+    public void setEndereco(Endereco endereco) { this.endereco = endereco; }
+
+    public Data getDataFundacao() { return dataFundacao; }
+    public void setDataFundacao(Data dataFundacao) { this.dataFundacao = dataFundacao; }
+
+
     @Override
     public String toString() {
         return "Loja: " + nome +
-                " | Funcionários: " + quantidadeFuncionarios +
-                " | Salário Base: R$" + salarioBaseFuncionario;
+                "\nFuncionários: " + quantidadeFuncionarios +
+                "\nSalário Base: R$" + salarioBaseFuncionario +
+                "\nEndereço: " + endereco +
+                "\nData de Fundação: " + dataFundacao;
     }
 
     public double gastosComSalario() {
